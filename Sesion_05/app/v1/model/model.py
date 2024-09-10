61abca654c0e6c04f124fe70caa090479ef55be0
Henry <henjacolonia17@gmail.com>
2024-09-09 21:38:16 -0500

    Sesion 05

---
+++ Sesion_05/app/v1/model/model.py
@@ -0,0 +1,17 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    city = Column(String, nullable=True)
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost:5433/inka_company'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+Base.metadata.create_all(bind=engine)
---
+++ Sesion_05/app/v1/schema/schema.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    city: str
+
+class UserCreate(UserBase):
+    pass
+
+class UserOut(UserBase):
+    id: UUID
+
+    class Config:
+        orm_mode = True
